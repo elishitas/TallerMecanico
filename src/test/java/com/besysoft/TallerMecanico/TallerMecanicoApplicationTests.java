4b68ff91dd0da1aadb95154305d22162763ce6c4
Elishitas <arayamariaelisa@gmail.com>
2023-03-13 19:05:04 -0300

    inicializando proyecto

---
+++ src/test/java/com/besysoft/TallerMecanico/TallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besysoft.TallerMecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
