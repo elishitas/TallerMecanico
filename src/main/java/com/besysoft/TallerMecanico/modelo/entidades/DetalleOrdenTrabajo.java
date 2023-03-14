e04bc209cb3f26b17db93e7187d4e399490f4ab9
Elishitas <arayamariaelisa@gmail.com>
2023-03-14 19:07:01 -0300

    se agrega entidad faltante

---
+++ src/main/java/com/besysoft/TallerMecanico/modelo/entidades/DetalleOrdenTrabajo.java
@@ -0,0 +1,12 @@
+package com.besysoft.TallerMecanico.modelo.entidades;
+
+import java.io.Serializable;
+
+public class DetalleOrdenTrabajo implements Serializable {
+
+    private Integer id;
+
+    private Integer cantidad;
+
+    private Double valorTotal;
+}
