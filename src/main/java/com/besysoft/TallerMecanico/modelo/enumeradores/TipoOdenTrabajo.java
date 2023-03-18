bdfe372c0038e73583387708ddf0919018c2b1c5
Elishitas <arayamariaelisa@gmail.com>
2023-03-18 15:12:53 -0300

    creo un nuevo enumerador para OrdenTrabajo

---
+++ src/main/java/com/besysoft/TallerMecanico/modelo/enumeradores/TipoOdenTrabajo.java
@@ -0,0 +1,14 @@
+package com.besysoft.TallerMecanico.modelo.enumeradores;
+
+public enum TipoOdenTrabajo {
+
+    CREADA,
+
+    EN_REPARACION,
+
+    PARA_FACTURAR,
+
+    FACTURADA,
+
+    CERRADA
+}
